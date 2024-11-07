7320af8e5cebbaff2e27e9e29b932f9d10c030db
aljersulit <aljeromesulit@gmail.com>
2024-11-07 16:40:42 +0800

    feat: add firebase authentication service

---
+++ src/services/authService.js
@@ -0,0 +1,10 @@
+import { auth } from '../firebase';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from 'firebase/auth';
+
+const signup = (email, password) => createUserWithEmailAndPassword(auth, email, password);
+
+const login = (email, password) => signInWithEmailAndPassword(auth, email, password);
+
+const logout = () => signOut(auth);
+
+export { signup, login, logout };
