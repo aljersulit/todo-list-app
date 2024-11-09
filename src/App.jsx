ed4b98421b3ecbfe03691fd748d0237c3a010cee
aljersulit <aljeromesulit@gmail.com>
2024-11-09 12:38:30 +0800

    wrap entire app with authentication context

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
+
 import Layout from "./components/Layout";
 import Login from "./components/Auth/Login"
 import Signup from "./components/Auth/Signup";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route path="login" element={<Login />} />
-          <Route path="signup" element={<Signup />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-    
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route path="login" element={<Login />} />
+            <Route path="signup" element={<Signup />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   )
 }
 
