2765a4515600445e397bfe202a79b3807c8984db
aljersulit <aljeromesulit@gmail.com>
2024-11-09 10:41:00 +0800

    feat: add authentication context for managing authentication state for the entire app

---
+++ src/context/AuthContext.js
@@ -0,0 +1,37 @@
+import { createContext, useContext, useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from '../firebase';
+
+
+const AuthContext = createContext();
+
+export const useAuth = () => useContext(AuthContext);
+
+export const AuthProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setCurrentUser(user);
+      setLoading(false);
+    });
+
+    return unsubscribe;
+  }, []);
+
+  const value = {
+    currentUser,
+  };
+
+  return (
+    <AuthContext.Provider value={value}>
+      {!loading && children}
+    </AuthContext.Provider>
+  );
+};
+
+AuthProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
--- src/services/authService.js
+++ src/services/authService.js
@@ -1,12 +1,22 @@
-import { auth } from '../firebaseConfig';
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, validatePassword} from 'firebase/auth';
+import { auth } from "../firebaseConfig";
+import {
+	createUserWithEmailAndPassword,
+	signInWithEmailAndPassword,
+	signOut,
+	validatePassword
+} from "firebase/auth";
 
-const signup = (email, password) => createUserWithEmailAndPassword(auth, email, password);
+const signup = (email, password) =>
+	createUserWithEmailAndPassword(auth, email, password);
 
-const login = (email, password) => signInWithEmailAndPassword(auth, email, password);
+const login = (email, password) =>
+	signInWithEmailAndPassword(auth, email, password);
 
 const logout = () => signOut(auth);
 
-const passwordValidate = (passwordFromUser) => validatePassword(auth, passwordFromUser);
+const passwordValidate = (passwordFromUser) =>
+	validatePassword(auth, passwordFromUser);
 
-export { signup, login, logout, passwordValidate };
+
+
+export { signup, login, logout, passwordValidate};
