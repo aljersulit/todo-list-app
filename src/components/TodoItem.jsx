1e1333a2391d19af88b3d6aca15c8bf46754a9dd
aljersulit <aljeromesulit@gmail.com>
2024-11-10 17:35:57 +0800

    feat: add todo title editable when edit button was clicked

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,14 +1,17 @@
-import PropTypes from 'prop-types';
-import { doc, updateDoc } from 'firebase/firestore';
-import { db } from '../firebaseConfig';
-function TodoItem({id, title, body, isCompleted}) {
+import { useState, useRef } from "react";
+import PropTypes from "prop-types";
+import { doc, updateDoc } from "firebase/firestore";
+import { db } from "../firebaseConfig";
+function TodoItem({ id, title, body, isCompleted }) {
+	const [isTitleEditable, setIsTitleEditable] = useState(false);
+	const titleInputRef = useRef(null);
 
-  async function toggleComplete() {
+	async function toggleComplete() {
 		try {
-			const docRef = doc(db, 'todos', id);
+			const docRef = doc(db, "todos", id);
 			await updateDoc(docRef, {
-				isCompleted: !isCompleted
-			})
+				isCompleted: !isCompleted,
+			});
 		} catch (error) {
 			alert("Something went wrong. Try again later");
 			console.error(error);
@@ -17,22 +20,58 @@ function TodoItem({id, title, body, isCompleted}) {
 
 	async function handleChangeNote(e) {
 		try {
-			const docRef = doc(db, 'todos', id);
+			const docRef = doc(db, "todos", id);
 			await updateDoc(docRef, {
-				body: e.target.value
-			})
+				body: e.target.value,
+			});
 		} catch (error) {
 			alert("Something went wrong. Try again later");
 			console.error(error);
 		}
 	}
 
+	async function handleUpdateTitle(e) {
+		try {
+			const docRef = doc(db, "todos", id);
+			await updateDoc(docRef, {
+				title: e.target.value,
+			});
+		} catch (error) {
+			alert("Something went wrong. Try again later");
+			console.error(error);
+		}
+	}
+
+	function handleEditTitle() {
+		setIsTitleEditable(true);
+
+		setTimeout(() => {
+			titleInputRef.current.focus();
+		}, 0);
+	}
+
 	return (
 		<li className="border-b border-gray-200">
 			<div className="flex items-center justify-between py-4">
 				<label className="flex items-center">
-					<input type="checkbox" className="mr-2" checked={isCompleted} onChange={toggleComplete}/>
-					<span>{title}</span>
+					<input
+						type="checkbox"
+						className="mr-2"
+						checked={isCompleted}
+						onChange={toggleComplete}
+					/>
+					{isTitleEditable ? (
+						<input
+							type="text"
+							ref={titleInputRef}
+							disabled={!isTitleEditable}
+							onBlur={() => setIsTitleEditable(false)}
+							value={title}
+							onChange={handleUpdateTitle}
+						/>
+					) : (
+						<span>{title}</span>
+					)}
 				</label>
 
 				<div>
@@ -45,6 +84,7 @@ function TodoItem({id, title, body, isCompleted}) {
 					<button
 						className="text-blue-500
                       hover:text-blue-700 edit-btn"
+						onClick={handleEditTitle}
 					>
 						Edit
 					</button>
@@ -54,7 +94,7 @@ function TodoItem({id, title, body, isCompleted}) {
 				rows={1}
 				placeholder="Notes..."
 				className="w-full"
-        value={body}
+				value={body}
 				onChange={handleChangeNote}
 			/>
 		</li>
@@ -65,7 +105,8 @@ export default TodoItem;
 
 TodoItem.propTypes = {
 	id: PropTypes.string.isRequired,
-  title: PropTypes.string.isRequired,
-  body: PropTypes.string,
-  isCompleted: PropTypes.bool.isRequired
+	title: PropTypes.string.isRequired,
+	body: PropTypes.string,
+	isCompleted: PropTypes.bool.isRequired,
 };
+
